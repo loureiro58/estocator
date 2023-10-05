13915c55b083705f049d829f37e387d855bee966
Matheus Loureiro <loureiro58@gmail.com>
2023-10-04 22:39:40 -0300

    Projeto gerado com Spring Initializr

---
+++ src/main/java/com/estocator/EstocatorApplication.java
@@ -0,0 +1,13 @@
+package com.estocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estocator/EstocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
